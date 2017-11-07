2a1cbef3b457dd5d73d276825a34e0e5183bc526
team3161 <team3161@gmail.com>
2017-11-07 10:38:52 -0500

    Update GearMechImpl.java

--- Offseason 2017/src/ca/team3161/impls/GearMechImpl.java
+++ Offseason 2017/src/ca/team3161/impls/GearMechImpl.java
@@ -2,11 +2,6 @@ package ca.team3161.impls;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-/** This is for the 2017 code package. That code is not on Github
- * I randomly created a package and when i know what the package name is, 
- * i will fix it then
- */
-
 public class GearMechImpl implements ca.team3161.interfaces.GearMech {
 	
 	private DoubleSolenoid claw;
@@ -19,57 +14,30 @@ public class GearMechImpl implements ca.team3161.interfaces.GearMech {
     claw.set(DoubleSolenoid.Value.kOff);
 	}
 
-	/**Classifying where the solenoids will be in PCM
-    */
-	
-    /**
-     * Release the flap
-     */
-	public void pullflap() {
-        flap.set(DoubleSolenoid.Value.kForward);
-    }
-
-    /**
-     * Set the Flap back in
-     */
-    public void returnflap() {
-        flap.set(DoubleSolenoid.Value.kReverse);
-    }
-
-    /**
-     * Open the Claw
-     */
-    public void openclaw() {
-        claw.set(DoubleSolenoid.Value.kForward);
-    }
-
-    /**
-     * Close the Claw
-     */
-    public void closeclaw() {
-        claw.set(DoubleSolenoid.Value.kReverse);
-    }
-
 	@Override
 	public void openClaw() {
+		claw.set(DoubleSolenoid.Value.kForward);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void closeClaw() {
+		 claw.set(DoubleSolenoid.Value.kReverse);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void openFlap() {
+		flap.set(DoubleSolenoid.Value.kForward);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void closeFlap() {
+		flap.set(DoubleSolenoid.Value.kReverse);
 		// TODO Auto-generated method stub
 		
 	}
